38d0206b1b668ba6d3eb42ae4ea65edfadd669b0
thilina01 <r.thilina@gmail.com>
2016-08-09 11:55:35 +0530

    Maven Project init
    Maven Project init added
    gitignore added

---
+++ src/main/java/com/trwlanka/FileManagerApiApplication.java
@@ -0,0 +1,12 @@
+package com.trwlanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerApiApplication.class, args);
+	}
+}
