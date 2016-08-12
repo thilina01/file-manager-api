8e7a01d899d85be4e07b2910072a9799103a4566
thilina01 <r.thilina@gmail.com>
2016-08-12 19:23:47 +0530

    folder feature enhancements
    base url changed from folder to folders
    functions for list all, find by id, delete by id added

--- src/main/java/com/trendsmixed/fma/controller/FolderController.java
+++ src/main/java/com/trendsmixed/fma/controller/FolderController.java
@@ -1,7 +1,11 @@
 package com.trendsmixed.fma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +16,30 @@ import com.trendsmixed.fma.service.FolderService;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/folder")
+@RequestMapping("/folders")
 public class FolderController {
 
 	@Autowired
 	private FolderService folderService;
-	
+
 	@PostMapping
-	public void save(@RequestBody Folder folder){
-		folderService.save(folder);
+	public Folder save(@RequestBody Folder folder){
+		return folderService.save(folder);
+	}
+
+	@GetMapping
+	public List<Folder> list(){
+		return folderService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Folder one(@PathVariable("id") int id){
+		return folderService.findOne(id);
+	}
+	
+	@GetMapping("/delete/{id}")
+	public void delete(@PathVariable("id") int id){
+		folderService.delete(id);
 	}
 
 }
