32df1cf80042b090318f203f621c764f66fafbce
thilina01 <r.thilina@gmail.com>
2016-08-12 19:24:13 +0530

    folder entity update
    description column added

--- src/main/java/com/trendsmixed/fma/entity/Folder.java
+++ src/main/java/com/trendsmixed/fma/entity/Folder.java
@@ -18,15 +18,12 @@ public class Folder {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
 	String name;
-	
+	String description;
 	@ManyToOne
 	Folder folder;
-	
-	@OneToMany(mappedBy="folder")
+	@OneToMany(mappedBy = "folder")
 	List<Folder> folders;
-
 	@ManyToMany
 	@JoinTable(name = "folder_file")
 	private List<File> files;
