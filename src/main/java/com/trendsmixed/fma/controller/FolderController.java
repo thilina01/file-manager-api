d2ff670a9d12c6b264840f1dd2cd7905c26bc7df
thilina01 <r.thilina@gmail.com>
2016-08-24 17:43:17 +0530

    Folder url mapping with options
    added functions to dandle varios optional request via url

--- src/main/java/com/trendsmixed/fma/controller/FolderController.java
+++ src/main/java/com/trendsmixed/fma/controller/FolderController.java
@@ -1,5 +1,6 @@
 package com.trendsmixed.fma.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trendsmixed.fma.entity.Folder;
+import com.trendsmixed.fma.jsonView.Views;
 import com.trendsmixed.fma.service.FolderService;
 
 @RestController
@@ -19,27 +21,62 @@ import com.trendsmixed.fma.service.FolderService;
 @RequestMapping("/folders")
 public class FolderController {
 
-	@Autowired
-	private FolderService folderService;
+    @Autowired
+    private FolderService folderService;
 
-	@PostMapping
-	public Folder save(@RequestBody Folder folder){
-		return folderService.save(folder);
-	}
+    @PostMapping
+    public Folder save(@RequestBody Folder folder) {
+        return folderService.save(folder);
+    }
 
-	@GetMapping
-	public List<Folder> list(){
-		return folderService.findAll();
-	}
+    @GetMapping//("/top")
+    public @JsonView(Views.FolderOnly.class) List<Folder> all() {
+        return folderService.findAll();
+    }
+    @GetMapping("/top")
+    public @JsonView(Views.FolderOnly.class) List<Folder> top() {
+        return folderService.findByFolderIsNull();
+    }
 
-	@GetMapping("/{id}")
-	public Folder one(@PathVariable("id") int id){
-		return folderService.findOne(id);
-	}
-	
-	@GetMapping("/delete/{id}")
-	public void delete(@PathVariable("id") int id){
-		folderService.delete(id);
-	}
+    @GetMapping("/{id}")
+    public @JsonView(Views.FolderOnly.class)
+    Folder one(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+    
+    @GetMapping("/{id}/with-parent")
+    public @JsonView(Views.FolderWithParent.class)
+    Folder oneWithParent(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+    
+    @GetMapping("/{id}/with-sub-folders")
+    public @JsonView(Views.FolderWithSubFolders.class)
+    Folder oneWithSubFolders(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+    
+    @GetMapping("/{id}/with-files")
+    public @JsonView(Views.FolderWithFiles.class)
+    Folder oneWithFiles(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+    
+    @GetMapping("/{id}/with-sub-folders-and-files")
+    public @JsonView(Views.FolderWithSubFoldersAndFiles.class)
+    Folder oneWithSubFoldersAndFiles(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+/*
+    @GetMapping("/{id}/with-parent-and-sub-folders-and-files")
+    public @JsonView(Views.FolderParentAndWithSubFoldersAndFiles.class)
+    Folder oneWithParentAndSubFoldersAndFiles(@PathVariable("id") int id) {
+        return folderService.findOne(id);
+    }
+*/
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable("id") int id) {
+        folderService.delete(id);
+    }
 
 }
