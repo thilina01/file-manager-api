f2e200abc44ad2abb15398c5621e4bf5400ed7ab
thilina01 <r.thilina@gmail.com>
2016-08-24 17:48:07 +0530

    added JsonView annotations
    added JsonView annotations to control data serialization and
    deserialidation

--- src/main/java/com/trendsmixed/fma/entity/Folder.java
+++ src/main/java/com/trendsmixed/fma/entity/Folder.java
@@ -1,5 +1,7 @@
 package com.trendsmixed.fma.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.trendsmixed.fma.jsonView.Views;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -14,17 +16,27 @@ import lombok.Data;
 
 @Entity
 @Data
+
 public class Folder {
-	@Id
-	@GeneratedValue
-	private Integer id;
-	String name;
-	String description;
-	@ManyToOne
-	Folder folder;
-	@OneToMany(mappedBy = "folder")
-	List<Folder> folders;
-	@ManyToMany
-	@JoinTable(name = "folder_file")
-	private List<File> files;
+
+    @Id
+    @GeneratedValue
+    @JsonView(Views.FolderOnly.class)
+    private Integer id;
+    @JsonView(Views.FolderOnly.class)
+    String name;
+    @JsonView(Views.FolderOnly.class)
+    String description;
+    @ManyToOne
+    @JsonView(Views.FolderWithParent.class)
+    Folder folder;
+
+    @JsonView(Views.FolderWithSubFolders.class)
+    @OneToMany(mappedBy = "folder")
+    List<Folder> folders;
+    
+    @JsonView(Views.FolderWithFiles.class)
+    @ManyToMany
+    @JoinTable(name = "folder_file")
+    private List<File> files;
 }
