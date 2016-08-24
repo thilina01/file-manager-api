b6e798eb2086d6879b12b165da3a99f92a4f403d
thilina01 <r.thilina@gmail.com>
2016-08-24 17:49:34 +0530

    JsonViews
    Defined json views to controll data serialization

---
+++ src/main/java/com/trendsmixed/fma/jsonView/Views.java
@@ -0,0 +1,19 @@
+package com.trendsmixed.fma.jsonView;
+public class Views {
+    public static interface FolderOnly {
+    }
+    public static interface FolderWithSubFolders extends FolderOnly {
+    }
+    public static interface FolderWithParent extends FolderOnly {
+    }
+    public static interface FolderWithFiles extends FolderOnly {
+    }
+    public static interface FolderWithSubFoldersAndFiles extends FolderWithSubFolders,FolderWithFiles {
+    }
+    public static interface FolderParentAndWithSubFoldersAndFiles extends FolderWithSubFolders,FolderWithFiles,FolderWithParent {
+    }
+    
+    public static interface Full extends FolderOnly {
+    }
+ 
+}
