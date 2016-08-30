2d934fb04037d4f2ec78a3fba6811409261a2f38
thilina01 <r.thilina@gmail.com>
2016-08-30 09:21:35 +0530

    Json View updates
    Added file related json views

--- src/main/java/com/trendsmixed/fma/jsonView/Views.java
+++ src/main/java/com/trendsmixed/fma/jsonView/Views.java
@@ -1,6 +1,9 @@
 package com.trendsmixed.fma.jsonView;
 public class Views {
+    
     public static interface FolderOnly {
+    }
+    public static interface FileOnly {
     }
     public static interface FolderWithSubFolders extends FolderOnly {
     }
@@ -10,7 +13,7 @@ public class Views {
     }
     public static interface FolderWithSubFoldersAndFiles extends FolderWithSubFolders,FolderWithFiles {
     }
-    public static interface FolderParentAndWithSubFoldersAndFiles extends FolderWithSubFolders,FolderWithFiles,FolderWithParent {
+    public static interface FolderParentAndWithSubFoldersAndFiles extends FolderWithSubFolders,FolderWithFiles,FolderWithParent,FileOnly {
     }
     
     public static interface Full extends FolderOnly {
