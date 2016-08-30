cbcf23e05dc8e42578aebd791bc203dada465797
thilina01 <r.thilina@gmail.com>
2016-08-30 09:23:16 +0530

    File related operations introduced
    Added functionalities to handle file related database operations

---
+++ src/main/java/com/trendsmixed/fma/repository/FileRepository.java
@@ -0,0 +1,10 @@
+package com.trendsmixed.fma.repository;
+
+import com.trendsmixed.fma.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FileRepository extends JpaRepository<File, Integer> {
+
+
+}
