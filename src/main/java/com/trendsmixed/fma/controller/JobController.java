bf779af7141916825e243e91877b927abf02d86f
Daminda <daminda4x@gmail.com>
2016-10-12 12:05:56 +0530

    Added Job Controller
    Added Job Controller to handle job related API calls

---
+++ src/main/java/com/trendsmixed/fma/controller/JobController.java
@@ -0,0 +1,53 @@
+package com.trendsmixed.fma.controller;
+
+import com.trendsmixed.fma.entity.AppSession;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trendsmixed.fma.entity.Customer;
+import com.trendsmixed.fma.entity.Job;
+import com.trendsmixed.fma.service.AppSessionService;
+import com.trendsmixed.fma.service.CustomerService;
+import com.trendsmixed.fma.service.JobService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/jobs")
+public class JobController {
+
+    @Autowired
+    private AppSessionService appSessionService;
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping
+    public List<Job> getMenus() {
+        return jobService.findAll();
+    }
+
+    @PostMapping
+    public Job save(@RequestBody Job job, @RequestHeader(value = "email", defaultValue = "") String email) {
+        AppSession appSession = appSessionService.findOne(email);
+        if (appSession == null) {
+            throw new Error("Unauthorized access");
+        } else {
+            try {
+                job = jobService.save(job);
+                return job;
+
+            } catch (Throwable e) {
+                while (e.getCause() != null) {
+                    e = e.getCause();
+                }
+                throw new Error(e.getMessage());
+            }
+        }
+    }
+}
