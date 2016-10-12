9aa22ebe2211efb3c35cbfde6c551fadffb4ad15
Daminda <daminda4x@gmail.com>
2016-10-12 14:47:01 +0530

    Added LossReason Controller
    Added LossReason Controller to handle loss reason related API calls

---
+++ src/main/java/com/trendsmixed/fma/controller/LossReasonController.java
@@ -0,0 +1,51 @@
+package com.trendsmixed.fma.controller;
+
+import com.trendsmixed.fma.entity.AppSession;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trendsmixed.fma.entity.LossReason;
+import com.trendsmixed.fma.service.AppSessionService;
+import com.trendsmixed.fma.service.LossReasonService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/lossReasons")
+public class LossReasonController {
+
+    @Autowired
+    private AppSessionService appSessionService;
+    @Autowired
+    private LossReasonService lossReasonService;
+
+    @GetMapping
+    public List<LossReason> getMenus() {
+        return lossReasonService.findAll();
+    }
+
+    @PostMapping
+    public LossReason save(@RequestBody LossReason lossReason, @RequestHeader(value = "email", defaultValue = "") String email) {
+        AppSession appSession = appSessionService.findOne(email);
+        if (appSession == null) {
+            throw new Error("Unauthorized access");
+        } else {
+            try {
+                lossReason = lossReasonService.save(lossReason);
+                return lossReason;
+
+            } catch (Throwable e) {
+                while (e.getCause() != null) {
+                    e = e.getCause();
+                }
+                throw new Error(e.getMessage());
+            }
+        }
+    }
+}
