8d61649c0d9171008623ba8dc06238f4f9063ad3
Daminda <daminda4x@gmail.com>
2016-10-19 16:24:19 +0530

    Json view enhancement

--- src/main/java/com/trendsmixed/fma/jsonView/CustomerView.java
+++ src/main/java/com/trendsmixed/fma/jsonView/CustomerView.java
@@ -12,65 +12,73 @@ public class CustomerView {
     }
 
     public static interface Consignee {
-
     }
 
     public static interface Contact {
-
     }
 
     public static interface Continent {
-
     }
 
     public static interface Fax {
-
     }
 
     public static interface FinalDestination {
-
     }
 
     public static interface NortifyParty {
-
     }
 
     public static interface Note {
-
     }
 
     public static interface OfficeAddress {
-
     }
 
     public static interface PaymentTerm {
-
     }
 
     public static interface PhoneNo {
-
     }
 
     public static interface SVatNo {
-
     }
 
     public static interface VatNo {
-
     }
 
     public static interface Incoterm {
-
     }
 
     public static interface CustType {
-
     }
 
     public static interface Country {
-
     }
 
     public static interface Currency {
+    }
+
+    public static interface All extends Id, Code, Name, Consignee, Contact, Continent, Fax, FinalDestination, NortifyParty, Note, OfficeAddress, PaymentTerm, PhoneNo, SVatNo, VatNo {
+
+    }
+
+    public static interface AllAndIncotermAll extends All, Incoterm, IncotermView.All {
+
+    }
+
+    public static interface AllAndCustTypeAll extends All, CustType, CustTypeView.All {
+
+    }
+
+    public static interface AllAndCountryAll extends All, Country, CountryView.All {
+
+    }
+
+    public static interface AllAndCurrencyAll extends All, Currency, CurrencyView.All {
+
+    }
+    public static interface AllAndIncotermAllAndCustTypeAllAndCountryAllAndCurrencyAll extends All, Incoterm,IncotermView.All,CustType,CustTypeView.All,Country,CountryView.All,Currency,CurrencyView.All {
+        
     }
 }
