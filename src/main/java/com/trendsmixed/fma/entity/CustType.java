862a5f9dacc65ddacb26ab521f6fe0297876bfb4
Daminda <daminda4x@gmail.com>
2016-10-19 10:47:20 +0530

    Jason views added
    CustType

--- src/main/java/com/trendsmixed/fma/entity/CustType.java
+++ src/main/java/com/trendsmixed/fma/entity/CustType.java
@@ -5,6 +5,8 @@
  */
 package com.trendsmixed.fma.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.trendsmixed.fma.jsonView.CustTypeView;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Basic;
@@ -33,10 +35,13 @@ public class CustType implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @JsonView(CustTypeView.Id.class)
     @Column(name = "id")
     private Integer id;
+    @JsonView(CustTypeView.Code.class)
     @Column(name = "code")
     private String code;
+    @JsonView(CustTypeView.Name.class)
     @Column(name = "name")
     private String name;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "custType")
