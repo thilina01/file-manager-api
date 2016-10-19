8c3bd106cb65301979d3e606140622ac076c227f
Daminda <daminda4x@gmail.com>
2016-10-19 10:41:30 +0530

    Jason views added
    Country

--- src/main/java/com/trendsmixed/fma/entity/Country.java
+++ src/main/java/com/trendsmixed/fma/entity/Country.java
@@ -5,6 +5,8 @@
  */
 package com.trendsmixed.fma.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.trendsmixed.fma.jsonView.CountryView;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Basic;
@@ -33,10 +35,13 @@ public class Country implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @JsonView(CountryView.Id.class)
     @Column(name = "id")
     private Integer id;
+    @JsonView(CountryView.Code.class)
     @Column(name = "code")
     private String code;
+    @JsonView(CountryView.Name.class)
     @Column(name = "name")
     private String name;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
