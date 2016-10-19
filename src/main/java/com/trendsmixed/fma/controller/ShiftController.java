fc0f1dacacf5d38b4317bea94a9c8070b6ddecfc
Daminda <daminda4x@gmail.com>
2016-10-19 10:25:27 +0530

    New API's added
    ShiftController
    ShiftView
    ShiftRepository
    ShiftService

---
+++ src/main/java/com/trendsmixed/fma/controller/ShiftController.java
@@ -0,0 +1,74 @@
+package com.trendsmixed.fma.controller;
+
+import com.trendsmixed.fma.entity.AppSession;
+import com.trendsmixed.fma.entity.Shift;
+import com.trendsmixed.fma.service.AppSessionService;
+import com.trendsmixed.fma.service.ShiftService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/shifts")
+public class ShiftController {
+
+    @Autowired
+    private ShiftService shiftService;
+    @Autowired
+    private AppSessionService appSessionService;
+
+    @PostMapping
+    public Shift save(@RequestBody Shift shift, @RequestHeader(value = "email", defaultValue = "") String email) {
+        AppSession appSession = appSessionService.findOne(email);
+        if (appSession == null) {
+            throw new Error("Unauthorized access");
+        } else {
+            try {
+                shift = shiftService.save(shift);
+
+                return shift;
+
+            } catch (Throwable e) {
+                while (e.getCause() != null) {
+                    e = e.getCause();
+                }
+                throw new Error(e.getMessage());
+            }
+        }
+    }
+
+//    @GetMapping
+//    @JsonView(ShiftView.IdAndCodeAndName.class)
+    public List<Shift> findAll() {
+        return shiftService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Shift findOne(@PathVariable("id") int id) {
+        return shiftService.findOne(id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String delete(@PathVariable int id) {
+        shiftService.delete(id);
+        return "Deleted";
+
+    }
+
+    @PutMapping("/{id}")
+    public Shift updateCustomer(@PathVariable int id, @RequestBody Shift shift) {
+        shift.setId(id);
+        shift = shiftService.save(shift);
+        return shift;
+    }
+}
---
+++ src/main/java/com/trendsmixed/fma/repository/ShiftRepository.java
@@ -0,0 +1,8 @@
+package com.trendsmixed.fma.repository;
+
+import com.trendsmixed.fma.entity.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftRepository extends JpaRepository<Shift, Integer> {
+
+}
