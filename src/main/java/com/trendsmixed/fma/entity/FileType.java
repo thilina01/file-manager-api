b987ded823991f34e276f4429f15241d40d95db9
Daminda <daminda4x@gmail.com>
2016-10-19 11:23:37 +0530

    Jason views added
    FileType

--- src/main/java/com/trendsmixed/fma/entity/FileType.java
+++ src/main/java/com/trendsmixed/fma/entity/FileType.java
@@ -5,6 +5,8 @@
  */
 package com.trendsmixed.fma.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.trendsmixed.fma.jsonView.FileTypeView;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Basic;
@@ -32,8 +34,10 @@ public class FileType implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @JsonView(FileTypeView.Id.class)
     @Column(name = "id")
     private Integer id;
+    @JsonView(FileTypeView.Name.class)
     @Column(name = "name")
     private String name;
     @OneToMany(mappedBy = "fileType")
@@ -94,5 +98,5 @@ public class FileType implements Serializable {
     public String toString() {
         return "com.trendsmixed.fma.entity.FileType[ id=" + id + " ]";
     }
-    
+
 }
