b2c761f0f2f038e91c6586ac45f33a5e407b2591
Daminda <daminda4x@gmail.com>
2016-10-20 16:48:59 +0530

    Added Purchase order type API

---
+++ src/main/java/com/trendsmixed/fma/controller/PurchaseOrderTypeController.java
@@ -0,0 +1,72 @@
+package com.trendsmixed.fma.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trendsmixed.fma.entity.PurchaseOrderType;
+import com.trendsmixed.fma.jsonView.PurchaseOrderTypeView;
+import com.trendsmixed.fma.service.AppSessionService;
+import com.trendsmixed.fma.service.PurchaseOrderTypeService;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/purchaseOrderTypes")
+public class PurchaseOrderTypeController {
+
+    @Autowired
+    private AppSessionService appSessionService;
+    @Autowired
+    private PurchaseOrderTypeService purchaseOrderTypeService;
+
+    @JsonView(PurchaseOrderTypeView.AlL.class)
+    @GetMapping
+    public List<PurchaseOrderType> findAll() {
+        return purchaseOrderTypeService.findAll();
+    }
+
+    @PostMapping
+    public PurchaseOrderType save(@RequestBody PurchaseOrderType purchaseOrderType, @RequestHeader(value = "email", defaultValue = "") String email, HttpServletRequest request) {
+        appSessionService.isValid(email, request);
+        try {
+            purchaseOrderType = purchaseOrderTypeService.save(purchaseOrderType);
+            return purchaseOrderType;
+
+        } catch (Throwable e) {
+            while (e.getCause() != null) {
+                e = e.getCause();
+            }
+            throw new Error(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public PurchaseOrderType findOne(@PathVariable("id") int id) {
+        return purchaseOrderTypeService.findOne(id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String delete(@PathVariable int id) {
+        purchaseOrderTypeService.delete(id);
+        return "Deleted";
+
+    }
+
+    @PutMapping("/{id}")
+    public PurchaseOrderType updateCustomer(@PathVariable int id, @RequestBody PurchaseOrderType purchaseOrderType) {
+        purchaseOrderType.setId(id);
+        purchaseOrderType = purchaseOrderTypeService.save(purchaseOrderType);
+        return purchaseOrderType;
+    }
+}
---
+++ src/main/java/com/trendsmixed/fma/jsonView/PurchaseOrderTypeView.java
@@ -0,0 +1,17 @@
+package com.trendsmixed.fma.jsonView;
+
+public class PurchaseOrderTypeView {
+
+    public static interface Id {
+    }
+
+    public static interface Type {
+    }
+
+    public static interface Code {
+    }
+
+    public static interface AlL extends Id, Type, Code {
+    }
+
+}
