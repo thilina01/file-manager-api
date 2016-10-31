049809022602d96a0a1d84dd4df2d7b7b1db932f
thilina01 <r.thilina@gmail.com>
2016-10-31 09:51:04 +0530

    Removed unwanted entity and related files

--- src/main/java/com/trendsmixed/fma/repository/RunDateDefectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trendsmixed.fma.repository;
-
-import com.trendsmixed.fma.entity.RunDateDefect;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RunDateDefectRepository extends JpaRepository<RunDateDefect, Integer> {
-
-}
--- src/main/java/com/trendsmixed/fma/repository/ScrapTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trendsmixed.fma.repository;
-
-import com.trendsmixed.fma.entity.ScrapType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScrapTypeRepository extends JpaRepository<ScrapType, Integer> {
-
-}
