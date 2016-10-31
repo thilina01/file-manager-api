2dd768c4bf6918efa87ad4ee05afc4b78e932681
Daminda <daminda4x@gmail.com>
2016-10-31 10:16:03 +0530

    New API's added

---
+++ src/main/java/com/trendsmixed/fma/repository/LossTypeRepository.java
@@ -0,0 +1,8 @@
+package com.trendsmixed.fma.repository;
+
+import com.trendsmixed.fma.entity.LossType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LossTypeRepository extends JpaRepository<LossType, Integer> {
+
+}
---
+++ src/main/java/com/trendsmixed/fma/service/LossTypeService.java
@@ -0,0 +1,32 @@
+package com.trendsmixed.fma.service;
+
+import com.trendsmixed.fma.entity.LossType;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trendsmixed.fma.repository.LossTypeRepository;
+
+@Service
+public class LossTypeService {
+
+    @Autowired
+    private LossTypeRepository LossTypeRepository;
+
+    public List<LossType> findAll() {
+        return LossTypeRepository.findAll();
+    }
+
+    public LossType save(LossType lossType) {
+        return LossTypeRepository.save(lossType);
+    }
+
+    public LossType findOne(int id) {
+        return LossTypeRepository.findOne(id);
+    }
+
+    public void delete(int id) {
+        LossTypeRepository.delete(id);
+    }
+}
