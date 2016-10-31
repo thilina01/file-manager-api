ca102eaa431fc35bb90b9232f84d3cc724af40a3
thilina01 <r.thilina@gmail.com>
2016-10-31 13:50:03 +0530

    Downtme entity and related files removed

--- src/main/java/com/trendsmixed/fma/repository/DownTimeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trendsmixed.fma.repository;
-
-import com.trendsmixed.fma.entity.DownTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DownTimeRepository extends JpaRepository<DownTime, Integer> {
-
-}
