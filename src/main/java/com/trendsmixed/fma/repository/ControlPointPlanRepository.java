9473f36783df735c9653de561de3298d47e0e602
Daminda <daminda4x@gmail.com>
2016-11-03 11:05:26 +0530

    New API's added
    ControlPointPlan

---
+++ src/main/java/com/trendsmixed/fma/repository/ControlPointPlanRepository.java
@@ -0,0 +1,8 @@
+package com.trendsmixed.fma.repository;
+
+import com.trendsmixed.fma.entity.ControlPointPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ControlPointPlanRepository extends JpaRepository<ControlPointPlan, Integer> {
+
+}
