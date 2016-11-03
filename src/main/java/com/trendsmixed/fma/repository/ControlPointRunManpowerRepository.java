16e8d224afc9a9e915af5be0a030eb7a5f4dd2cf
Daminda <daminda4x@gmail.com>
2016-11-03 15:09:08 +0530

    New API's added

---
+++ src/main/java/com/trendsmixed/fma/repository/ControlPointRunManpowerRepository.java
@@ -0,0 +1,8 @@
+package com.trendsmixed.fma.repository;
+
+import com.trendsmixed.fma.entity.ControlPointRunManpower;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ControlPointRunManpowerRepository extends JpaRepository<ControlPointRunManpower, Integer> {
+
+}
---
+++ src/main/java/com/trendsmixed/fma/service/ControlPointRunManpowerService.java
@@ -0,0 +1,33 @@
+package com.trendsmixed.fma.service;
+
+import com.trendsmixed.fma.entity.ControlPointRunManpower;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trendsmixed.fma.repository.ControlPointRunManpowerRepository;
+
+@Service
+public class ControlPointRunManpowerService {
+
+    @Autowired
+    private ControlPointRunManpowerRepository controlPointRunManpowerRepository;
+
+    public List<ControlPointRunManpower> findAll() {
+        return controlPointRunManpowerRepository.findAll();
+    }
+
+    public ControlPointRunManpower save(ControlPointRunManpower ControlPointRunManpower) {
+        return controlPointRunManpowerRepository.save(ControlPointRunManpower);
+    }
+
+    public ControlPointRunManpower findOne(int id) {
+        return controlPointRunManpowerRepository.findOne(id);
+    }
+
+    public void delete(int id) {
+        controlPointRunManpowerRepository.delete(id);
+    }
+}
+
