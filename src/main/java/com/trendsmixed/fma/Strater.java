e3cf77611079838bc6b49f6ee2cee00202f5fb60
Thilina Ranathunga <r.thilina@gmail.com>
2017-01-25 13:47:17 +0530

    Added some sample data

--- src/main/java/com/trendsmixed/fma/Strater.java
+++ src/main/java/com/trendsmixed/fma/Strater.java
@@ -1,6 +1,14 @@
 package com.trendsmixed.fma;
 
+import com.trendsmixed.fma.entity.Country;
+import com.trendsmixed.fma.entity.Currency;
+import com.trendsmixed.fma.entity.Incoterm;
+import com.trendsmixed.fma.entity.SaleType;
 import com.trendsmixed.fma.entity.Status;
+import com.trendsmixed.fma.service.CountryService;
+import com.trendsmixed.fma.service.CurrencyService;
+import com.trendsmixed.fma.service.IncotermService;
+import com.trendsmixed.fma.service.SaleTypeService;
 import com.trendsmixed.fma.service.StatusService;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -12,9 +20,27 @@ public class Strater {
 
     @Autowired
     private StatusService statusService;
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private CurrencyService currencyService;
+    @Autowired
+    private IncotermService incotermService;
+    @Autowired
+    private SaleTypeService saleTypeService;
 
     @PostConstruct
     public void afterStarted() {
+        System.out.println("Init process started");
+        initStatus();
+        initCountry();
+        initCurrency();
+        initIncoterm();
+        initSaleType();
+        System.out.println("Init process finished");
+    }
+
+    private void initStatus() {
         List<Status> statuses = statusService.findAll();
         if (statuses.isEmpty()) {
             Status active = new Status();
@@ -26,6 +52,46 @@ public class Strater {
             statuses.add(inactive);
 
             statusService.save(statuses);
+        }
+    }
+
+    private void initCountry() {
+        Country country = countryService.findByCode("NA");
+        if (country == null) {
+            country = new Country();
+            country.setCode("NA");
+            country.setName("NOT AVAILABLE");
+            countryService.save(country);
+        }
+    }
+
+    private void initCurrency() {
+        Currency currency = currencyService.findByCode("NA");
+        if (currency == null) {
+            currency = new Currency();
+            currency.setCode("NA");
+            currency.setName("NOT AVAILABLE");
+            currencyService.save(currency);
+        }
+    }
+
+    private void initIncoterm() {
+        Incoterm incoterm = incotermService.findByCode("NA");
+        if (incoterm == null) {
+            incoterm = new Incoterm();
+            incoterm.setCode("NA");
+            incoterm.setName("NOT AVAILABLE");
+            incotermService.save(incoterm);
+        }
+    }
+    
+    private void initSaleType() {
+        SaleType saleType = saleTypeService.findByCode("NA");
+        if (saleType == null) {
+            saleType = new SaleType();
+            saleType.setCode("NA");
+            saleType.setName("NOT AVAILABLE");
+            saleTypeService.save(saleType);
         }
     }
 }
