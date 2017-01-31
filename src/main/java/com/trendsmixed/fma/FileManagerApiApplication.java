beb4dbc4ccc585ce481a0503e7cf6bc61a0d029a
Thilina Ranathunga <r.thilina@gmail.com>
2017-01-31 09:18:17 +0530

    changes to run on tomcat
    changed file to enable run on tomcat

--- src/main/java/com/trendsmixed/fma/FileManagerApiApplication.java
+++ src/main/java/com/trendsmixed/fma/FileManagerApiApplication.java
@@ -2,9 +2,16 @@ package com.trendsmixed.fma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FileManagerApiApplication {
+public class FileManagerApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FileManagerApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FileManagerApiApplication.class, args);
