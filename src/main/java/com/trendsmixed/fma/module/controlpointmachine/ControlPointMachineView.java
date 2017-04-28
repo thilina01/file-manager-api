0b36ea23adac5f4347a3f651b753e3828c448863
Thilina Ranathunga <r.thilina@gmail.com>
2017-04-28 16:15:37 +0530

    JSON View enhancements

--- src/main/java/com/trendsmixed/fma/module/controlpointmachine/ControlPointMachineView.java
+++ src/main/java/com/trendsmixed/fma/module/controlpointmachine/ControlPointMachineView.java
@@ -1,17 +1,23 @@
 package com.trendsmixed.fma.module.controlpointmachine;
 
+import com.trendsmixed.fma.module.controlpoint.ControlPointView;
+import com.trendsmixed.fma.module.machine.MachineView;
+
 public class ControlPointMachineView {
 
-    public static interface Id {
-    }
+	public static interface Id {
+	}
 
-    public static interface ControlPoint {
+	public static interface ControlPoint extends ControlPointView.All {
 
-    }
+	}
 
-    public static interface Machine {
+	public static interface Machine extends MachineView.All {
 
-    }
+	}
 
-    
+	public interface ControlPointAndMachine extends ControlPoint, Machine {
+
+	}
+
 }
