cbf218e939f269cf8d1634565b4139f859c06216
Thilina Ranathunga <r.thilina@gmail.com>
2017-06-12 14:44:14 +0530

    DAO for chart MonthlyScheduleAdherence added

---
+++ src/main/java/com/trendsmixed/fma/dao/MonthlyScheduleAdherence.java
@@ -0,0 +1,15 @@
+package com.trendsmixed.fma.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MonthlyScheduleAdherence {
+
+    String month;
+    long actualTotal;
+    long plannedTotal;
+    long adherence;
+
+}
