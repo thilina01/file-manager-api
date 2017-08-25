b8099a6ceab0626e4cc766a9bbe983487c79612a
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-08-25 08:48:28 +0530

    Problem Fixed

--- src/main/java/com/trendsmixed/fma/module/sectiontype/SectionTypeRepository.java
+++ src/main/java/com/trendsmixed/fma/module/sectiontype/SectionTypeRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface SectionTypeRepository extends PagingAndSortingRepository<SectionType,Integer> {
 
     public SectionType findByCode(String code);
+    
+    public SectionType findByName(String name);
 
     @Query(value = "SELECT"
-            + " new com.trendsmixed.fma.dao.Combo(o.id, o.code, o.description)"
-            + " FROM Item o")
+            + " new com.trendsmixed.fma.dao.Combo(o.id, o.code, o.name)"
+            + " FROM SectionType o")
     public List<Combo> getCombo();
 
 }
