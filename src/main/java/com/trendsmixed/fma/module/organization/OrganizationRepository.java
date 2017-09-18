84fb0da5e85c2617b14effeb7b2a64c00bccd55a
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-09-18 10:19:40 +0530

    Organization API added

---
+++ src/main/java/com/trendsmixed/fma/module/organization/OrganizationRepository.java
@@ -0,0 +1,18 @@
+package com.trendsmixed.fma.module.organization;
+
+import com.trendsmixed.fma.dao.Combo;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrganizationRepository extends PagingAndSortingRepository<Organization, Integer> {
+
+    public Organization findByCode(String code);
+
+    public Organization findByName(String name);
+
+    @Query(value = "SELECT"
+            + " new com.trendsmixed.fma.dao.Combo(o.id, o.code, o.name)"
+            + " FROM Organization o")
+    public List<Combo> getCombo();
+}
