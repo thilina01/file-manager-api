bdd89fa4231ecfc52b761c06873e76be7f25d91a
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-10-09 11:04:19 +0530

    On Time Delivery API Added

---
+++ src/main/java/com/trendsmixed/fma/module/ontimedelivery/OnTimeDeliveryRepository.java
@@ -0,0 +1,15 @@
+package com.trendsmixed.fma.module.ontimedelivery;
+
+import com.trendsmixed.fma.dao.Combo;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OnTimeDeliveryRepository extends PagingAndSortingRepository<OnTimeDelivery, Integer> {
+
+    @Query(value = "SELECT"
+            + " new com.trendsmixed.fma.dao.Combo(o.id, '', '')"
+            + " FROM OnTimeDelivery o")
+    public List<Combo> getCombo();
+
+}
