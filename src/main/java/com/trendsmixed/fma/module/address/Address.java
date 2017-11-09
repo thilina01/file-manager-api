804860370057570b2e82d6051bc45101807d1978
Thilina Ranathunga <r.thilina@gmail.com>
2017-11-09 10:32:55 +0530

    Remove incorrect unique attribute

--- src/main/java/com/trendsmixed/fma/module/address/Address.java
+++ src/main/java/com/trendsmixed/fma/module/address/Address.java
@@ -31,19 +31,19 @@ public class Address implements Serializable {
     @Column(name = "id")
     private Integer id;
     @JsonView(AddressView.Line1.class)
-    @Column(name = "line1", unique = true)
+    @Column(name = "line1")
     private String line1;
     @JsonView(AddressView.Line2.class)
-    @Column(name = "line2", unique = true)
+    @Column(name = "line2")
     private String line2;
     @JsonView(AddressView.Line3.class)
-    @Column(name = "line3", unique = true)
+    @Column(name = "line3")
     private String line3;
     @JsonView(AddressView.Line4.class)
-    @Column(name = "line4", unique = true)
+    @Column(name = "line4")
     private String line4;
     @JsonView(AddressView.Line5.class)
-    @Column(name = "line5", unique = true)
+    @Column(name = "line5")
     private String line5;
     @JsonView(AddressView.AddressType.class)
     @JoinColumn(name = "address_type_id", referencedColumnName = "id")
