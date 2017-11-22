c154cb3a35f8d1846bde72c84b7798a7321e8d9d
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-11-22 09:48:06 +0530

    Add new column for invoice

--- src/main/java/com/trendsmixed/fma/module/invoice/Invoice.java
+++ src/main/java/com/trendsmixed/fma/module/invoice/Invoice.java
@@ -49,8 +49,9 @@ public class Invoice implements Serializable {
     @JsonView(InvoiceView.InvoiceDispatchNote.class)
     @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "invoice")
     private List<InvoiceDispatchNote> invoiceDispatchNoteList;
-    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "invoice")
-    private List<PackingList> packingListList;
+    @JoinColumn(name = "packing_list_id", referencedColumnName = "id")
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, optional = true)
+    private PackingList packingList;
 
     public Invoice(Integer id) {
         this.id = id;
