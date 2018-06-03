55384c0b547777ed64d8942e1e7b8754b52bd57f
Umesha <umesha.nipunialawatta@gmail.com>
2018-06-03 12:21:53 +0530

    Fix: dispatch sechedule search problem

--- src/main/java/com/trendsmixed/fma/module/dispatchschedule/DispatchScheduleController.java
+++ src/main/java/com/trendsmixed/fma/module/dispatchschedule/DispatchScheduleController.java
@@ -60,70 +60,27 @@ public class DispatchScheduleController {
     }
     
     @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/dispatchScheduleDurationPage", params = {"startDate", "endDate"})
-    public Page<DispatchSchedule> dispatchScheduleDurationPage(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderOrderDateBetween(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
-    }
-    @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/orderDateAndJobPage", params = {"orderDate", "job"})
-    public Page<DispatchSchedule> orderDateAndJobPage(@RequestParam("orderDate") String orderDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderOrderDateAndJob(Format.yyyy_MM_dd.parse(orderDate), new Job(Integer.valueOf(job)), pageable));
-    }
-    @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/dispatchScheduleDurationAndJobPage", params = {"startDate", "endDate", "job"})
-    public Page<DispatchSchedule> dispatchScheduleDurationAndJobPage(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderOrderDateBetweenAndJob(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
-    }
-    @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/customerAndDispatchScheduleDurationPage", params = {"customer", "startDate", "endDate"})
-    public Page<DispatchSchedule> customerAndDispatchScheduleDurationPage(@RequestParam("customer") String customer, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetween(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
-    }
-    @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/customerAndOrderDateAndJobPage", params = {"customer", "orderDate", "job"})
-    public Page<DispatchSchedule> customerAndOrderDateAndJobPage(@RequestParam("customer") String customer, @RequestParam("orderDate") String orderDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateAndJob(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(orderDate), new Job(Integer.valueOf(job)), pageable));
-    }
-    @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
-    @GetMapping(value = "/customerAndDispatchScheduleDurationAndJobPage", params = {"customer", "startDate", "endDate", "job"})
-    public Page<DispatchSchedule> customerAndDispatchScheduleDurationAndJobPage(@RequestParam("customer") String customer, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
-    }
-
-
-    @GetMapping(value = "/itemAndOrderDateDurationPage", params = {"item", "startDate", "endDate"})
-    public Page<DispatchSchedule> jobAndOrderDateDurationPage(@RequestParam("item") String item, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, Pageable pageable) throws ParseException {
-        return new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetween(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
-    }
-
-    @GetMapping(value = "/salesOrderTypeAndOrderDateDurationPage", params = {"salesOrderType", "startDate", "endDate"})
-    public Page<DispatchSchedule> salesOrderTypeAndOrderDateDurationPage(@RequestParam("salesOrderType") String salesOrderType, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetween(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
-    }
-
-    @GetMapping(value = "/itemAndDispatchScheduleDurationAndJobPage", params = {"item", "startDate", "endDate", "job"})
-    public Page<DispatchSchedule> itemAndOrderDateDurationAndJobPage(@RequestParam("item") String item, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
-    }
-
-    @GetMapping(value = "/itemAndDispatchScheduleDurationAndSalesOrderTypePage", params = {"item", "startDate", "endDate", "salesOrderType"})
-    public Page<DispatchSchedule> itemAndOrderDateDurationAndSalesOrderTypePage(@RequestParam("item") String item, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("salesOrderType") String salesOrderType, Pageable pageable) throws ParseException {
-        return new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderSalesOrderType(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new SalesOrderType(Integer.valueOf(salesOrderType)), pageable));
-    }
-
-    @GetMapping(value = "/itemAndDispatchScheduleDurationAndCustomerPage", params = {"item", "startDate", "endDate", "customer"})
-    public Page<DispatchSchedule> itemAndOrderDateDurationAndCustomerPage(@RequestParam("item") String item, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("customer") String customer, Pageable pageable) throws ParseException {
-        return new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderCustomer(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Customer(Integer.valueOf(customer)), pageable));
-    }
-
-    @GetMapping(value = "/salesOrderTypeAndDispatchScheduleDurationAndJobPage", params = {"salesOrderType", "startDate", "endDate", "job"})
-    public Page<DispatchSchedule> salesOrderTypeAndOrderDateDurationAndJobPage(@RequestParam("salesOrderType") String salesOrderType, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("job") String job, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
-    }
-
-    @GetMapping(value = "/salesOrderTypeAndDispatchScheduleDurationAndCustomerPage", params = {"salesOrderType", "startDate", "endDate", "customer"})
-    public Page<DispatchSchedule> salesOrderTypeAndOrderDateDurationAndCustomerPage(@RequestParam("salesOrderType") String salesOrderType, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("customer") String customer, Pageable pageable) throws ParseException {
-        return new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderCustomer(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Customer(Integer.valueOf(customer)), pageable));
+    @GetMapping(value = "/dispatchSchedule")
+    public Page<DispatchSchedule> getDispatchSchedule(
+        @RequestParam(value = "customer", required = false, defaultValue = "0") String customer,
+        @RequestParam(value = "job", required = false, defaultValue = "0") String job,   
+        @RequestParam(value = "startDate", required = false, defaultValue = "1970-01-01") String startDate,
+        @RequestParam(value = "endDate", required = false, defaultValue = "2100-12-31") String endDate, 
+        Pageable pageable) throws ParseException {
+        Page<DispatchSchedule> page ;
+        if(customer.equals("0")&& job.equals("0") ){
+            page = new Page(service.findBySalesOrderItemSalesOrderOrderDateBetween(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
+        } 
+        else if(customer.equals("0") ){
+            page = new Page(service.findBySalesOrderItemSalesOrderOrderDateBetweenAndJob(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
+        }
+        else if(job.equals("0") ){
+            page = new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetween(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
+        }
+        else {
+            page = new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
+        }
+        return page;
     }
 
     @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerAllCustomerItemAllAndJobAllAndItemAll.class)
@@ -159,50 +116,38 @@ public class DispatchScheduleController {
         if(customer.equals("0") && job.equals("0") && item.equals("0") && salesOrderType.equals("0")){
             page = new Page(service.findBySalesOrderItemSalesOrderOrderDateBetween( Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
         } 
-
         else if(job.equals("0") && item.equals("0") && salesOrderType.equals("0")){
             page = new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetween(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
         }
-
         else if(customer.equals("0") && item.equals("0") && salesOrderType.equals("0")){
-        page = new Page(service.findBySalesOrderItemSalesOrderOrderDateBetweenAndJob(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
+            page = new Page(service.findBySalesOrderItemSalesOrderOrderDateBetweenAndJob(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
         }
-
         else if(customer.equals("0") && job.equals("0") && salesOrderType.equals("0")){
             page = new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetween(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
         }
-
         else if(customer.equals("0") && job.equals("0") && item.equals("0")){
             page = new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetween(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
         }
         else if(customer.equals("0") && salesOrderType.equals("0")){
             page = new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
         }
-
         else if(customer.equals("0") && job.equals("0")){
             page = new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderSalesOrderType(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new SalesOrderType(Integer.valueOf(salesOrderType)), pageable));
         }
-
         else if(job.equals("0")  && salesOrderType.equals("0")){
             page = new Page(service.findByJobItemAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderCustomer(new Item(Integer.valueOf(item)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Customer(Integer.valueOf(customer)), pageable));
         }
-
         else if(item.equals("0") && salesOrderType.equals("0")){
             page = new Page(service.findBySalesOrderItemSalesOrderCustomerAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
         }
-        
         else if(customer.equals("0") && item.equals("0") ){
             page = new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetweenAndJob(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Job(Integer.valueOf(job)), pageable));
         }
-
         else{
             page = new Page(service.findBySalesOrderItemSalesOrderSalesOrderTypeAndSalesOrderItemSalesOrderOrderDateBetweenAndSalesOrderItemSalesOrderCustomer(new SalesOrderType(Integer.valueOf(salesOrderType)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), new Customer(Integer.valueOf(customer)), pageable));
         }
-        
         return page;
     }
-
-
 
     @JsonView(DispatchScheduleView.AllAndSalesOrderItemAllAndSalesOrderAllCustomerItemAllAndJobAllAndItemAll.class)
     @PostMapping
