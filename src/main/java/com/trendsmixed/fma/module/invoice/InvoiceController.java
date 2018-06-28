047cf8a4b0657f78b89306c830898993eda107e0
Umesha <umesha.nipunialawatta@gmail.com>
2018-06-28 12:45:10 +0530

    Add new View

--- src/main/java/com/trendsmixed/fma/module/invoice/InvoiceController.java
+++ src/main/java/com/trendsmixed/fma/module/invoice/InvoiceController.java
@@ -58,6 +58,24 @@ public class InvoiceController {
     public Page<Invoice> customerPage( @RequestParam("customer") String customer, Pageable pageable) throws ParseException {
         return new Page(service.findByCustomer(new Customer(Integer.valueOf(customer)), pageable));
     }
+    
+    @JsonView(InvoiceView.AllAndDispatchNoteAndLoadingPlanAndLoadingPlanItemAndDispatchScheduleAndJobAndItemAndSalesOrderItemAndSalesOrderAndCustomerItemAndPackagingSpecificationAndPortOfLoadingAndContainerSizeAndAddressAndCustomerAndIncotermAndInvoiceType.class)
+    @GetMapping(value = "/customerAndInvoiceDateBetween")
+    public Page<Invoice> getCustomerAndInvoiceDateBetweenPage(
+        @RequestParam(value = "customer", required = false, defaultValue = "0") String customer,
+        @RequestParam(value = "startDate", required = false, defaultValue = "1970-01-01") String startDate,
+        @RequestParam(value = "endDate", required = false, defaultValue = "2100-12-31") String endDate, 
+        Pageable pageable) throws ParseException {
+        Page<Invoice> page ;
+
+        if(customer.equals("0")){
+            page = new Page(service.findByInvoiceDateBetween(Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));    
+        } 
+        else{
+            page = new Page(service.findByCustomerAndInvoiceDateBetween(new Customer(Integer.valueOf(customer)), Format.yyyy_MM_dd.parse(startDate), Format.yyyy_MM_dd.parse(endDate), pageable));
+        }
+        return page;
+    }
 
     @JsonView(InvoiceView.All.class)
     @PostMapping
